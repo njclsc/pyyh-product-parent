afb14c53392f3e10c874f833a8a2231ec7fe0bcd
njclsc <77166776@qq.com>
2024-03-06 23:24:14 +0800

    协议解析完毕

---
+++ pyyh-product-zh-collection/src/main/java/com/zh/collection/business/handler/DataOperateHandler.java
@@ -0,0 +1,27 @@
+package com.zh.collection.business.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DataOperateHandler extends ChannelInboundHandlerAdapter{
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		// TODO Auto-generated method stub
+		String data = String.valueOf(msg);
+		String deviceId = data.substring(4, 8);
+		String cmdType = data.substring(8, 10);
+		int tagNum = Integer.parseInt(data.substring(12, 14), 16);
+		for(int i = 0; i < tagNum; i++){
+			String tagData = data.substring(i * 16 + 14, (i + 1) * 16 + 14);
+			String tagId = tagData.substring(0, 6);
+			String status = tagData.substring(6, 8);
+			String antId = tagData.substring(8, 12);
+			String lowrsi = tagData.substring(12, 14);
+			String hrsi = tagData.substring(14, 16);
+			System.out.println(tagId + "  " + status + "  " + antId + "  " + lowrsi + "  " + hrsi);
+		}
+		System.out.println(deviceId + "  " + cmdType + "  " + tagNum);
+	}
+
+}
--- pyyh-product-zh-collection/src/main/java/com/zh/collection/lizer/UDPChannelinitializer.java
+++ pyyh-product-zh-collection/src/main/java/com/zh/collection/lizer/UDPChannelinitializer.java
@@ -1,5 +1,6 @@
 package com.zh.collection.lizer;
 
+import com.zh.collection.business.handler.DataOperateHandler;
 import com.zh.collection.business.handler.TestHandler;
 import com.zh.collection.business.handler.ZHUDPDecoder;
 
@@ -11,7 +12,7 @@ public class UDPChannelinitializer extends ChannelInitializer<DatagramChannel>{
 	@Override
 	protected void initChannel(DatagramChannel arg0) throws Exception {
 		// TODO Auto-generated method stub
-		arg0.pipeline().addLast(new ZHUDPDecoder()).addLast(new TestHandler());
+		arg0.pipeline().addLast(new ZHUDPDecoder()).addLast(new DataOperateHandler());
 	}
 
 }
